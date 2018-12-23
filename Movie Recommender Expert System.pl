e60d8318b1b8578e0e64962aefed0eb72e2551e6
Ashtiwddn <40496169+Ashtiwddn@users.noreply.github.com>
2018-12-23 18:05:54 +0530

    Add files via upload

---
+++ Movie Recommender Expert System.pl
@@ -0,0 +1,172 @@
+
+movie('Zielona mila','Frank Darabont',1999,'drama', 'others',  188,'USA','Tom Hanks',8.7,719).
+movie('Pif Paf! Jestes trup', 'Guy Ferland', 2002, 'drama', 'others',  87, 'Kanada', 'Ben Foster', 7.7, 16).
+movie('Dogville', 'Lars von Trier', 2003, 'drama', 'others', 178, 'Dania', 'Nicole Kidman', 7.7, 45).
+movie('Z dystansu', 'Ton Kayne', 2011, 'drama', 'others', 100, 'USA', 'Adrien Brody', 7.9, 30).
+movie('Lista Schindlera', 'Steven Spielberg', 1993, 'drama', 'others', 195, 'USA', 'Adrien Brody', 8.4, 259).
+movie('Requiem dla snu', 'Darren Aronofsky', 2000, 'drama', 'others', 102, 'USA', 'Jared Leto', 7.9, 520).
+movie('Biutiful', 'Alejandro Gonzalez Inarritu', 2010, 'drama', 'others', 148, 'Hiszpania', 'Javier Bardem', 7.6, 12).
+movie('Czarny labedz', 'Darren Aronofsky', 2010, 'drama', 'others', 108, 'USA', 'Natalie Portman', 7.7, 248).
+movie('Gladiator', 'Ridley Scott', 2000, 'drama', 'others', 155, 'USA', 'Russell Crowe', 8.1, 552).
+movie('Dzien swira', 'Marek Koterski', 2002, 'drama', 'others', 123, 'Polska', 'Marek Kondrat', 7.8, 438).
+movie('Pianista', 'Roman Polanski', 2002, 'drama', 'others', 150, 'Polska', 'Adrien Brody', 8.3, 410).
+/** COMEDY */
+movie('Seksmisja', 'Juliusz Machulski', 1984, 'comedy', 'others', 118, 'Polska', 'Jerzy Stuhr', 7.9, 420).
+movie('Forrest Gump', 'Robert Zemeckis', 1994, 'comedy', 'others', 144, 'USA', 'Tom Hanks', 8.6, 697).
+movie('Kac Vegas', 'Todd Phillips', 2009, 'comedy', 'others', 100, 'USA', 'Bradley Cooper', 7.3, 537).
+movie('Notykalni', 'Olivier Nakache', 2011, 'comedy', 'others', 112, 'Francja', 'Francois Cluzet', 8.7, 393).
+movie('Truman Show', 'Peter Weir', 1998, 'comedy', 'others', 103, 'USA', 'Jim Carrey', 7.4, 383).
+movie('Kiler', 'Juliusz Machulski', 1997, 'comedy', 'others', 104, 'Polska', 'Cezary Pazura', 7.7, 315).
+movie('Kevin sam w domu', 'Chris Columbus', 1990, 'comedy', 'others', 103, 'USA', 'Macaulay Culkin', 7.1, 297).
+movie('Mis', 'Stanislaw Bareja', 1980, 'comedy', 'others', 111, 'Polska', 'Stanislaw Tym', 7.8, 261).
+movie('Diabel ubiera sie u Prady', 'David Frankel', 2006, 'comedy', 'others', 109, 'USA', 'Meryl Streep', 6.9, 227).
+movie('Jak rozpetalem druga wojne swiatowa', 'Tadeusz Chmielewski', 1969, 'comedy', 'others', 236, 'Polska', 'Marian Kociniak', 7.9, 195).
+/** ADVENTURE */
+movie('Wladca Pierscieni: Druzyna Pierscienia', 'Peter Jackson', 2001, 'adventure', 'fantasy', 178, 'Nowa Zelandia', 'Elijah Wood', 8.1, 503).
+movie('Sherlock Holmes', 'Guy Ritchie', 2009, 'adventure', 'family', 128, 'Nomcy', 'Robert Downey Jr', 7.8, 317).
+movie('Harry Potter i Kamien Filozoficzny', 'Chris Columbus', 2001, 'adventure', 'fantasy', 152, 'Wielka Brytania', 'Danol Radcliffe', 7.4, 269).
+movie('Hobbit: Nozwykla podroz', 'Peter Jackson', 2012, 'adventure', 'fantasy', 169, 'Nowa Zelandia', 'Martin Freeman', 7.7, 269).
+movie('Piraci z Karaibow: Skrzynia umarlaka', 'Gore Verbinski', 2006, 'adventure', 'fantasy', 150, 'USA', 'Johnny Depp', 7.8, 295).
+movie('Matrix', 'Andy Wachowski', 1999, 'adventure', 'scifi', 136, 'USA', 'Keanu Reeves', 7.6, 629).
+movie('Powrot do przyszlosci', 'Robert Zemeckis', 1985, 'adventure', 'scifi', 116, 'USA', 'Michael J. Fox', 7.3, 265).
+movie('W pustyni i w puszczy', 'Gavin Hood', 2001, 'adventure', 'family', 110, 'Polska', 'Karolina Sawka', 5.6, 48).
+movie('Wiedzmin', 'Marek Brodzki', 2001, 'adventure', 'fantasy', 130, 'Polska', 'Michal Zebrowski', 3.9, 37).
+movie('Gwiezdne wojny: Czesc IV - Nowa nadzieja', 'George Lucas', 1977, 'adventure', 'scifi', 121, 'USA', 'Mark Hamill', 8.0, 199).
+/** THRILLER */
+movie('Milczeno owiec', 'Jonathan Demme', 1991, 'thriller', 'others', 118, 'USA', 'Anthony Hopkins', 8.2, 474).
+movie('Kod da Vinci', 'Ron Howard', 2006, 'thriller', 'others', 149, 'USA', 'Tom Hanks', 6.6, 447).
+movie('Szosty zmysl', 'M. Night Shyamalan', 1999, 'thriller', 'others', 107, 'USA', 'Bruce Willis', 7.9, 482).
+movie('Podzmieny krag', 'David Fincher', 1999, 'thriller', 'others', 139, 'Nomcy', 'Brad Pitt', 8.3, 426).
+movie('Se7en', 'David Fincher', 1995, 'thriller', 'others', 127, 'USA', 'Morgan Freeman', 8.3, 409).
+movie('Wyspa tajemnic', 'Martin Scorsese', 2010, 'thriller', 'others', 138, 'USA', 'Leonardo Di Caprio', 8.1, 374).
+movie('Adwokat diabla', 'Taylor Hackford', 1997, 'thriller', 'others', 144, 'Nomcy', 'Keanu Reeves', 8.0, 317).
+movie('Sala samobojcow', 'Jan Komasa', 2011, 'thriller', 'others', 117,'Polska',  'Jakub Gierszal', 6.5, 223).
+movie('American Psycho', 'Mary Harron', 2000, 'thriller', 'others',  101,'USA', 'Christian Bale', 7.3, 105).
+movie('Inni', 'Alejandro Amenabar', 2001, 'thriller', 'others', 101,'Francja',  'Nicole Kidman', 7.4, 166).
+/** ANIMATION */
+movie('Shrek', 'Andrew Adamson', 2001, 'animation', 'others', 90, 'USA', 'Mike Myers', 7.8, 604).
+movie('Epoka lodowcowa', 'Chris Wedge', 2002, 'animation', 'others', 81, 'USA', 'Cezary Pazura', 7.6, 359).
+movie('Krol lew', 'Rob Minkoff', 1994, 'animation', 'others', 89, 'USA', 'Matthew Broderick', 8.3, 381).
+movie('Madagaskar', 'Tom McGrath', 2005, 'animation', 'others', 86, 'USA', 'Ben Stiller', 7.5, 290).
+movie('Odlot', 'Pete Docter', 2009, 'animation', 'others', 96, 'USA', 'Edward Asner', 7.4, 161).
+movie('Gdzie jest Nemo?', 'Andrew Stanton', 2003, 'animation', 'others', 100, 'Australia', 'Alexander Gould', 7.1, 126).
+movie('Toy Story', 'John Lasseter', 1995, 'animation', 'others', 80, 'USA', 'Tom Hanks', 7.4, 124).
+movie('Kosmiczny mecz', 'Joe Pytka', 1996, 'animation', 'others', 88, 'USA', 'Michael Jordan', 6.9, 100).
+movie('Miasteczko South Park', 'Trey Parker', 1999, 'animation', 'others', 81, 'USA', 'Trey Parker', 7.2, 59).
+movie('Tarzan', 'Chris Buck', 1999, 'animation', 'others', 88, 'USA', 'Tony Goldwyn', 7.4, 42).
+/** ROMANCE */
+movie('Czekolada', 'Lasse Hallstrom', 2000, 'romance', 'others', 117, 'USA', 'Juliette Binoche', 7.5, 164).
+movie('Step Up - Tanoc zmyslow', 'Anne Fletcher', 2006, 'romance', 'others', 104, 'USA', 'Channing Tatum', 7.0, 174).
+movie('Czas na milosc', 'Richard Curtis', 2013, 'romance', 'others', 119, 'Wielka Brytania', 'Domhnall Gleeson', 7.6, 100).
+movie('500 dni milosci', 'Marc Webb', 2009, 'romance', 'others', 95, 'USA', 'Joseph Gordon-Levitt', 7.2, 99).
+movie('Lepiej pozno niz poznoj', 'Nancy Meyers', 2003, 'romance', 'others', 128, 'USA', 'Jack Nicholson', 7.0, 102).
+movie('Amelia', 'Jean-Pierre Jeunet', 2001, 'romance', 'others', 129, 'Francja', 'Audrey Tautou', 7.6, 203).
+movie('To wlasno milosc', 'Richard Curtis', 2003, 'romance', 'others', 135, 'USA', 'Colin Firth', 7.7, 173).
+movie('Dziennik Bridget Jones', 'Sharon Maguire', 2001, 'romance', 'others', 97, 'Wielka Brytania', 'Renee Zellweger', 6.9, 204).
+movie('Tylko mno kochaj', 'Ryszard Zatorski', 2006, 'romance', 'others', 98, 'Polska', 'Agnoszka Grochowska', 5.8, 137).
+movie('Nigdy w zyciu!', 'Ryszard Zatorski', 2004, 'romance', 'others', 100, 'Polska', 'Danuta Stenka', 6.2, 129).
+/** HORROR */
+movie('Lsnono', 'Stanley Kubrick', 1980, 'horror', 'psychopaths', 146, 'USA', 'Jack Nicholson', 7.9, 293).
+movie('Jestem legenda', 'Francis Lawrence', 2007, 'horror', 'monsters', 101, 'Australia', 'Will Smith', 7.5, 344).
+movie('Wywiad z wampirem', 'Neil Jordan', 1994, 'horror', 'monsters', 123, 'USA', 'Brad Pitt', 7.8, 243).
+movie('Obcy - 8. pasazer Nostromo', 'Ridley Scott', 1979, 'horror', 'monsters', 117, 'USA', 'Sigourney Weaver', 7.9, 211).
+movie('The Ring', 'Gore Verbinski', 2002, 'horror', 'ghosts', 115, 'Japonia', 'Naomi Watts', 7.0, 173).
+movie('Pila', 'James Wan', 2004, 'horror', 'psychopaths', 103, 'Australia', 'Tobin Bell', 6.7, 172).
+movie('Oszukac przeznaczeno', 'James Wong', 2000, 'horror', 'others', 98, 'Kanada', 'Devon Sawa', 6.6, 158).
+movie('Dziewiate wrota', 'Roman Polanski', 1999, 'horror', 'others', 133, 'Francja', 'Johnny Depp', 7.3, 148).
+movie('Obecnosc', 'James Wan', 2013, 'horror', 'ghosts', 112, 'USA', 'Vera Farmiga', 7.4, 116).
+movie('Sierocinoc', 'Juan Antonio Bayona', 2007, 'horror', 'ghosts', 105, 'Hiszpania', 'Belen Rueda', 7.0, 119).
+movie('Rytual', 'Mikael Hafstrom', 2011, 'horror', 'ghosts', 114, 'USA', 'Anthony Hopkins', 6.8, 84).
+
+
+
+
+
+
+
+
+
+
+
+
+go(Y) :-	
+                write('MOVIE RECOMMENDER EXPERT SYSTEM'),nl,
+                write('Please answer questions below . Please enter all answers in small letters. '),nl,nl,
+
+               	write('   What is your name? Enter in double quotes (for example- "Tim") '),nl,
+               	read(Name),nl,
+               	write('   Hello '),
+				write(Name),nl,
+				write('   What is your gender? (FOR EXAMPLE- male)'),nl,
+				write('1. male'),nl,
+				write('2. female'),nl,
+				read(Sex),nl,
+				write('   How is your mood now? (FOR EXAMPLE- happy) '),nl,
+				write('1. Sad'),nl,
+				write('2. Happy'),nl,
+				write('3. Normal'),nl,
+				read(Mood),nl,
+				
+	          write('Movies after the year- ?'),nl,
+                  read(Ans1),nl,
+		write('Choose your genre.(FOR EXAMPLE- drama):'),nl,
+                write('According to your mood we would like to recommend following movie genre -- '),
+				recommendation(Sex, Mood, Genre),
+				write(Genre),nl,nl,
+				write('1. Animation'),nl,
+				write('2. Drama'),nl,
+				write('3. Horror'),nl,
+				write('4. Comedy'),nl,
+				write('5. Romance'),nl,
+				write('6. Thriller'),nl,
+				write('7. Adventure'),nl,
+				read(Ans2),nl,
+			write('   How many time do you have?'),nl,
+				write('1. Little (LESS THAN 100 MINUTES)'),nl,
+				write('2. Many'),nl,
+				read(C1),nl,
+				(  C1 == 'many' ->
+				    write('   Do you really have a lot of time? '),nl,
+				    write('1. Yes (MORE THAN 3 HOURS)'),nl,
+					write('2. No (LESS THAN 3 HOURS)'),nl,
+					read(C2),nl,
+					(C2 == 'no' -> Ans3 = 'long' ; Ans3 = 'verylong')
+					;
+				    Ans3 = 'short'
+				),
+			write('Minimum imdb rating that you would prefer?(ON THE SCALE OF 1 TO 10)'),nl,
+			read(Ans4),nl,
+                      (  year(Y,Ans1,Ans2,Ans3,Ans4) ; write('No such movie in the database.Please reduce your preferences'),nl,
+			write('What do you want to omit ? '),nl,nl,
+			write('1.movieyear'),nl,write('2.Duration'),nl,write('3.imdb rating'),nl,nl,
+			read(A2),nl,
+			(
+			A2 == 'movieyear' ->nomovieyear(Y,omit,Ans2,Ans3,Ans4) ; (A2== 'duration' ->noduration(Y,Ans1,Ans2,omit,Ans4) ; noimdb(Y,Ans1,Ans2,Ans3,omit) )		
+			)
+
+                         ; write('According to your preferences we would recommend you-'),nl,onlygenre(Y,Ans2)
+		      ).
+
+imdb(Y,Z)                               :- movie(Y,A,B,C,D,E,F,G,X,H),X>Z.
+year(Y,A1):-movie(Y,A,X,B,C,D,E,F,G,H), A1 < X.
+
+recommendation(X, Y, O) 		:- (X == 'female', Y == 'happy'), O = 'comedy, adventure, romance'. 
+recommendation(X, Y, O) 		:- (X == 'female', Y == 'sad'), O = 'drama, animation'. 
+recommendation(X, Y, O) 		:- (X == 'female', Y == 'normal'), O = 'horror, animation, romance'.
+recommendation(X, Y, O) 		:- (X == 'male', Y == 'happy'), O = 'comedy, adventure, thriller'.
+recommendation(X, Y, O) 		:- (X == 'male', Y == 'sad'), O = 'drama, comedy'. 
+recommendation(X, Y, O) 		:- (X == 'male', Y == 'normal'), O = 'horror, animation, thriller'.
+
+duration(Y,O) 				:- movie(Y,A,B,C,D,X,E,F,G,H), 20 < X, X < 90, O =='short'.
+duration(Y,O)				:- movie(Y,A,B,C,D,X,E,F,G,H), 91 < X, X < 170, O =='long'.
+duration(Y,O)				:- movie(Y,A,B,C,D,X,E,F,G,H), 171< X, O =='verylong'.
+
+nomovieyear(Y,omit,B,C,D):-checky(Y,B,C,D).
+noduration(Y,A,B,omit,D):-checkd(Y,A,B,D).
+noimdb(Y,A,B,C,omit):-checki(Y,A,B,C).
+year(Y,A,B,C,D):-check(Y,A,B,C,D).
+onlygenre(Y,Ans2):-movie(Y,P,Q,B,R,S,T,U,V,W).
+checky(Y,B,C,D):-movie(Y,P,Q,B,R,S,T,U,V,W),imdb(Y,D),duration(Y,C), !.
+checkd(Y,A,B,D):-movie(Y,P,Q,B,R,S,T,U,V,W),year(Y,A),imdb(Y,D), !.
+checki(Y,A,B,C):-movie(Y,P,Q,B,R,S,T,U,V,W),year(Y,A),duration(Y,C), !.
+check(Y,A,B,C,D):-movie(Y,P,Q,B,R,S,T,U,V,W),year(Y,A),imdb(Y,D),duration(Y,C), !.
